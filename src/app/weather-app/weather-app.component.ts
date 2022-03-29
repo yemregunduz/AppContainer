c6c3b580cfa3ca9794076c2986a6863c19cf8ee6
yemregunduz <yegunduz@outlook.com>
2022-03-29 16:28:18 +0300

    console.log deleted

--- src/app/weather-app/weather-app.component.ts
+++ src/app/weather-app/weather-app.component.ts
@@ -5,34 +5,27 @@ import { WeatherAppService } from './services/weather-app.service';
 @Component({
   selector: 'app-weather-app',
   templateUrl: './weather-app.component.html',
-  styleUrls: ['./weather-app.component.css']
+  styleUrls: ['./weather-app.component.css'],
 })
 export class WeatherAppComponent implements OnInit {
-
-  mainCities:string[]=["İstanbul","London","Berlin","Paris"]
-  weatherOfMainCities: WeatherModel[]=[];
-  city:string=""
-  constructor(private weatherService:WeatherAppService) { }
-  weather:WeatherModel = new WeatherModel()
+  mainCities: string[] = ['İstanbul', 'London', 'Berlin', 'Paris'];
+  weatherOfMainCities: WeatherModel[] = [];
+  city: string = '';
+  constructor(private weatherService: WeatherAppService) {}
+  weather: WeatherModel = new WeatherModel();
   ngOnInit(): void {
-    this.getWeatherOfMainCities()
-   
-    
+    this.getWeatherOfMainCities();
   }
-  getWeatherData(city:string){
-    this.weatherService.getWeatherData(city).subscribe(response=>{
-      this.weather=response
-    })
+  getWeatherData(city: string) {
+    this.weatherService.getWeatherData(city).subscribe((response) => {
+      this.weather = response;
+    });
   }
-  getWeatherOfMainCities(){
-    this.mainCities.forEach(city=>{
-      this.weatherService.getWeatherData(city).subscribe(response=>{
-        this.weatherOfMainCities.push(response)
-        console.log(this.weatherOfMainCities)
-      })
-    }
-    )
+  getWeatherOfMainCities() {
+    this.mainCities.forEach((city) => {
+      this.weatherService.getWeatherData(city).subscribe((response) => {
+        this.weatherOfMainCities.push(response);
+      });
+    });
   }
-  
-
 }
