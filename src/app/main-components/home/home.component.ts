5aeb65bae3486f0ddea8054210f2c642120175f7
yemregunduz <yegunduz@outlook.com>
2022-03-31 23:57:33 +0300

    chart app colors and png updated

--- src/app/main-components/home/home.component.ts
+++ src/app/main-components/home/home.component.ts
@@ -17,8 +17,7 @@ export class HomeComponent implements OnInit {
     {name:"Weather App",primaryColor:'white',secondaryColor:"#4B515D",description:"You can get weather information from this application.", srcImage:"./assets/images/weather-app.png",routerLink:"weather-app"},
     {name:"GitRepos App",primaryColor:'#ff5000',secondaryColor:"#1379bb",description:"You can list users' public repositories on github.", srcImage:"./assets/images/github-repos-app.png",routerLink:"githubrepos-app"},
     {name:"Notes App",primaryColor:'white',secondaryColor:"#f4aeba",description:"You can save and delete notes.", srcImage:"./assets/images/notes-app.png",routerLink:"notes-app"},
-    {name:"Chart App",primaryColor:'#AAAA',secondaryColor:"#79c2d0",description:"You can create dynamic charts with the column names in the excel you have uploaded.", srcImage:"./assets/images/chart-app.png",routerLink:"chart-app"},
-
+    {name:"Chart App",primaryColor:' #393e46',secondaryColor:"#79c2d0",description:"You can create dynamic charts with the column names in the excel you have uploaded.", srcImage:"./assets/images/chart-app.png",routerLink:"chart-app"},
   ]
   ngOnInit(): void {
   }
