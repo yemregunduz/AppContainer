dc0afe8b464f33516cd59ba783fb250aabe243a8
yemregunduz <yegunduz@outlook.com>
2022-04-03 18:27:07 +0300

    markdown app colors' updated

--- src/app/main-components/home/home.component.ts
+++ src/app/main-components/home/home.component.ts
@@ -18,9 +18,9 @@ export class HomeComponent implements OnInit {
     {name:"BMI Calculator",primaryColor:'#a9c25d',secondaryColor:"#AAAAAA",description:"You can calculate your body mass index.", srcImage:"./assets/images/bmiCalculator-app.png",routerLink:"bmicalculator-app"},
     {name:"Notes App",primaryColor:'white',secondaryColor:"#f4aeba",description:"You can save and delete notes.", srcImage:"./assets/images/notes-app.png",routerLink:"notes-app"},
     {name:"Random Quotes",primaryColor:'#8d9db6',secondaryColor:"#b6bcc2cb",description:"You can generate a random quote for sharing", srcImage:"./assets/images/randomQuotes-app.png",routerLink:"randomquotes-app"},
-    {name:"Markdown Editor App",primaryColor:'#347474',secondaryColor:" #ffcbcb",description:"A basic markdown previewer and downloader", srcImage:"./assets/images/markdownEditor-app.png",routerLink:"markdowneditor-app"},
+    {name:"Markdown Editor App",primaryColor:'  #ffcbcb',secondaryColor:"#347474 ",description:"A basic markdown previewer and downloader", srcImage:"./assets/images/markdownEditor-app.png",routerLink:"markdowneditor-app"},
     {name:"GitRepos App",primaryColor:'#ff5000',secondaryColor:"#1379bb",description:"You can list users' public repositories on github.", srcImage:"./assets/images/github-repos-app.png",routerLink:"githubrepos-app"},
-
+  
   ]
   ngOnInit(): void {
   }
