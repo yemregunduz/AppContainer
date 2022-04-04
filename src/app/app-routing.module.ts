8c53c79a81e230491ccee7eaac3871e3e7d94d5e
yemregunduz <yegunduz@outlook.com>
2022-04-04 06:00:00 +0300

    file directories changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BmiComponent } from './bmi-app/bmi/bmi.component';
-import { ChartAppComponent } from './chart-app/chart-app.component';
-import { GithubReposAppComponent } from './github-repos-app/github-repos-app.component';
-import { HomeComponent } from './main-components/home/home.component';
-import { MarkdownEditorAppComponent } from './markdown-editor-app/markdown-editor-app.component';
-import { MovieAppLayoutComponent } from './movie-app/movie-app-layout/movie-app-layout.component';
-import { NotesAppComponent } from './notes-app/notes-app.component';
-import { RandomQuotesComponent } from './random-quotes/random-quotes.component';
-import { TenFingersAppComponent } from './ten-fingers-app/ten-fingers-app.component';
-import { TodoAppComponent } from './todo-app/todo-app/todo-app.component';
-import { WeatherAppComponent } from './weather-app/weather-app.component';
+import { BmiComponent } from './components/bmi-app/bmi/bmi.component';
+import { ChartAppComponent } from './components/chart-app/chart-app.component';
+import { GithubReposAppComponent } from './components/github-repos-app/github-repos-app.component';
+import { HomeComponent } from './components/main-components/home/home.component';
+import { MarkdownEditorAppComponent } from './components/markdown-editor-app/markdown-editor-app.component';
+import { MovieAppLayoutComponent } from './components/movie-app/movie-app-layout/movie-app-layout.component';
+import { NotesAppComponent } from './components/notes-app/notes-app.component';
+import { RandomQuotesComponent } from './components/random-quotes/random-quotes.component';
+import { TenFingersAppComponent } from './components/ten-fingers-app/ten-fingers-app.component';
+import { TodoAppComponent } from './components/todo-app/todo-app/todo-app.component';
+import { WeatherAppComponent } from './components/weather-app/weather-app.component';
 
 const routes: Routes = [
   {path:"",component:HomeComponent},
   {path:"movie-app",component:MovieAppLayoutComponent,children:[
-    {path:"popular-movies",loadChildren:()=> import("./movie-app/movie-app-components/popular-movies/popular-movies.module")
+    {path:"popular-movies",loadChildren:()=> import("./components/movie-app/movie-app-components/popular-movies/popular-movies.module")
     .then(module=>module.PopularMoviesModule)},
-    {path:"favourite-movies",loadChildren :() => import("./movie-app/movie-app-components/favourite-movies/favourite-movies.module")
+    {path:"favourite-movies",loadChildren :() => import("./components/movie-app/movie-app-components/favourite-movies/favourite-movies.module")
     .then(module=>module.FavouriteMoviesModule)},
     
   ]},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoAppComponent } from './todo-app/todo-app/todo-app.component';
+import { TodoAppComponent } from './components/todo-app/todo-app/todo-app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { HomeComponent } from './main-components/home/home.component';
-import { BmiComponent } from './bmi-app/bmi/bmi.component';
-import { RandomQuotesComponent } from './random-quotes/random-quotes.component';
+import { HomeComponent } from './components/main-components/home/home.component';
+import { BmiComponent } from './components/bmi-app/bmi/bmi.component';
+import { RandomQuotesComponent } from './components/random-quotes/random-quotes.component';
 import { HttpClientModule } from '@angular/common/http';
-import { WeatherAppComponent } from './weather-app/weather-app.component';
-import { DegreePipe } from './weather-app/pipes/degree.pipe';
-import { DateFormat } from './weather-app/pipes/date.pipe';
-import { MovieAppModule } from './movie-app/movie-app.module';
-import { GithubReposAppComponent } from './github-repos-app/github-repos-app.component';
-import { NotesAppComponent } from './notes-app/notes-app.component';
-import { FilterPipe } from './notes-app/pipes/filter.pipe';
-import { ChartAppComponent } from './chart-app/chart-app.component';
-import { ReadExcelDirective } from './chart-app/directives/read-excel.directive';
+import { WeatherAppComponent } from './components/weather-app/weather-app.component';
+import { DegreePipe } from './components/weather-app/pipes/degree.pipe';
+import { DateFormat } from './components/weather-app/pipes/date.pipe';
+import { MovieAppModule } from './components/movie-app/movie-app.module';
+import { GithubReposAppComponent } from './components/github-repos-app/github-repos-app.component';
+import { NotesAppComponent } from './components/notes-app/notes-app.component';
+import { FilterPipe } from './components/notes-app/pipes/filter.pipe';
+import { ChartAppComponent } from './components/chart-app/chart-app.component';
+import { ReadExcelDirective } from './components/chart-app/directives/read-excel.directive';
 import { ToastrModule } from 'ngx-toastr';
-import { MarkdownEditorAppComponent } from './markdown-editor-app/markdown-editor-app.component';
-import { SanitizeHtmlPipe } from './markdown-editor-app/pipes/sanitize-html.pipe';
-import { TenFingersAppComponent } from './ten-fingers-app/ten-fingers-app.component';
+import { MarkdownEditorAppComponent } from './components/markdown-editor-app/markdown-editor-app.component';
+import { SanitizeHtmlPipe } from './components/markdown-editor-app/pipes/sanitize-html.pipe';
+import { TenFingersAppComponent } from './components/ten-fingers-app/ten-fingers-app.component';
 
 
 
